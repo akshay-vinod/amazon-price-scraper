d381e139c74032499c7bf110a155af0a9e5ad57a
akshay-vinod <akshayvinod468@gmail.com>
2020-08-14 18:27:41 +0530

    mail message on lower price

--- scrape.py
+++ scrape.py
@@ -1,40 +1,51 @@
 import requests
 from bs4 import BeautifulSoup
 import smtplib
+import time
+def check_price():
+    url = "https://www.amazon.in/PB10IZM/dp/B07VZRJQX1/ref=sr_1_6?crid=2V5YIWPXLY1D6&dchild=1&keywords=boat+power+bank&qid=1597386015&sprefix=boat+p%2Caps%2C464&sr=8-6"
 
-url = "https://www.amazon.in/PB10IZM/dp/B07VZRJQX1/ref=sr_1_6?crid=2V5YIWPXLY1D6&dchild=1&keywords=boat+power+bank&qid=1597386015&sprefix=boat+p%2Caps%2C464&sr=8-6"
+    headers = {
+        "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36'}
+        
+    page = requests.get(url, headers=headers)
 
-headers = {
-    "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36'}
-    
-page = requests.get(url, headers=headers)
+    soup =  BeautifulSoup(page.content, 'html.parser') #loaded site content
 
-soup =  BeautifulSoup(page.content, 'html.parser') #loaded site content
+    title = soup.find(id="productTitle").get_text() #fetched item name
 
-title = soup.find(id="productTitle").get_text() #fetched item name
+    price = soup.find(id="priceblock_ourprice").get_text() #fetch item price
+    modprice = ''.join(filter(lambda i: i.isdigit(), price))
+    converted_price = float(modprice[0:3])
+    print((converted_price))
+    print(title.strip())
 
-price = soup.find(id="priceblock_ourprice").get_text() #fetch item price
+    if (converted_price < 899.0): 
+        send_mail()  
+    else:
+        print("NO CHANGE IN PRICE")
+def send_mail():
+        # creates SMTP session
+        s = smtplib.SMTP('smtp.gmail.com', 587)
 
-price_converted = price[0 : 5]
+        # start TLS for security
+        s.starttls()
 
-print(price_converted)
+        # Authentication
+        s.login("sender_email_id", "sender_email_id_password") 
 
-print(title.strip())
+        # message to be sent
+        message = "PRICE CHANGED CHECK NOW "
 
-# creates SMTP session
-s = smtplib.SMTP('smtp.gmail.com', 587)
+        # sending the mail
+        s.sendmail("sender_email_id", "receiver_email_id", message)
 
-# start TLS for security
-s.starttls()
+        # terminating the session
+        s.quit()
+        print("email has been send")
+while(True):
+    check_price()
+    time.sleep(1) #check price in every 1 min
 
-# Authentication
-s.login("akshayvinod73@gmail.com", "dtgnfetkzqfachbv")
 
-# message to be sent
-message = "test message"
-
-# sending the mail
-s.sendmail("akshayvinod73@gmail.com", "athiravinod964@gmail.com", message)
-
-# terminating the session
-s.quit()
+        
